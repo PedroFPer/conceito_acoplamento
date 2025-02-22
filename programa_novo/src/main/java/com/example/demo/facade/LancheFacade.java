a40d560e84b101740be85ba1d2907bf5247f581a
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-02-22 15:03:14 -0300

    Resolução de exercicio

---
+++ programa_novo/src/main/java/com/example/demo/facade/LancheFacade.java
@@ -0,0 +1,38 @@
+package com.example.demo.facade;
+
+import com.example.demo.applications.LancheApplication;
+import com.example.demo.entities.Lanche;
+
+import java.util.List;
+
+public class LancheFacade {
+    private LancheApplication lancheApplication;
+
+    public LancheFacade(LancheApplication lancheApplication) {
+        this.lancheApplication = lancheApplication;
+    }
+
+    public void cadastrar(Lanche lanche) {
+        this.lancheApplication.cadastrar(lanche);
+    }
+
+    public List<Lanche> buscar() {
+        return this.lancheApplication.buscar();
+    }
+
+    public Lanche buscarPorCodigo(int codigo) {
+        return this.lancheApplication.buscarPorCodigo(codigo);
+    }
+
+    public double calcularLanche(Lanche lanche, int quantidade) {
+        return this.lancheApplication.calcularLanche(lanche, quantidade);
+    }
+
+    public void excluir(int codigo){
+        this.lancheApplication.remover(codigo);
+    }
+
+    public void atualizar(int codigo, Lanche lanche){
+        this.lancheApplication.atualizar(codigo,lanche);
+    }
+}
