4467d34d505d0435ee96218bb9b7e33e3cbc3e41
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-03-07 14:45:38 -0300

    Adicionado opção de listar tabelas

--- front_end/scrips/menuLanche.js
+++ front_end/scrips/menuLanche.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputNomeLanche = document.getElementById("inputNomeLanche");
     const inputPrecoLanche = document.getElementById("inputPrecoLanche");
     const inputImgLanche = document.getElementById("inputImgLanche");
+    const formCadas = document.getElementById("formCadastro")
 
-
+    //CadastroLanche
     document.getElementById("btnCadastraLanche").addEventListener("click", function (event) {
 
         event.preventDefault();
@@ -42,5 +43,64 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.error("Erro:", error);
                 window.alert("Erro ao cadastrar o produto.");
             });
+    });
+
+    //Listar Lanche
+
+    async function listarLanches() {
+        try {
+            const resposta = await fetch("http://localhost:8080/api/listarLanche");
+            const dados = await resposta.json();
+
+            const tbody = document.querySelector("#tabelLanc tbody");
+
+            tbody.innerHTML = '';
+
+            dados.forEach(item => {
+                const linha = document.createElement("tr");
+
+                linha.classList.add("linhaTabela");
+
+                Object.values(item).forEach(valor => {
+                    const celula = document.createElement("td");
+                    celula.textContent = valor;
+                    linha.appendChild(celula);
+
+                    celula.classList.add("celulaTabela");
+                })
+                
+                tbody.appendChild(linha);
+            });
+            
+        } catch (error) {
+            console.error("Erro ao buscar dados:", erro);
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //Função Menu Hamburger
+    function menuHambu(toggleInfo) {
+        if (toggleInfo.style.display == "flex") {
+            toggleInfo.style.display = "none"
+        } else {
+            toggleInfo.style.display = "flex";
+        }
+    }
+
+    document.getElementById("btnCadas").addEventListener("click", () => menuHambu(formCadas));
+    document.getElementById("btnListarLanche").addEventListener("click", () => {
+        menuHambu(tabelLanc);
+        listarLanches();
     });
 });
